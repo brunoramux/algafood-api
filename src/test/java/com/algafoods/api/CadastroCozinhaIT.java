746fd4b27188445aac02284202cef49122abda82
Bruno Ramos Lemos <bruno.lemos@live.com>
2025-12-05 19:28:54 -0300

    Feature: database separada para testes com limpeza a cada teste

--- src/test/java/com/algafoods/api/CadastroCozinhaIT.java
+++ src/test/java/com/algafoods/api/CadastroCozinhaIT.java
@@ -5,17 +5,21 @@ import com.algafoods.api.domain.model.Cozinha;
 import com.algafoods.api.domain.model.Restaurante;
 import com.algafoods.api.domain.service.CadastroCozinhaService;
 import com.algafoods.api.domain.service.CadastroRestauranteService;
+import com.algafoods.api.util.DatabaseCleaner;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import jakarta.validation.ConstraintViolationException;
 import org.assertj.core.api.Assertions;
+import org.flywaydb.core.Flyway;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.math.BigDecimal;
@@ -24,6 +28,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(locations = "classpath:application-test.properties")
 public class CadastroCozinhaIT {
 
     @Autowired
@@ -34,6 +39,20 @@ public class CadastroCozinhaIT {
 
     @LocalServerPort
     private int port;
+
+    @Autowired
+    private DatabaseCleaner databaseCleaner;
+
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        RestAssured.port = port;
+        RestAssured.basePath = "/cozinhas";
+
+        databaseCleaner.clearTables();
+        prepareDatabase();
+    }
 
     @Test
     public void cadastroCozinhaComSucessoTest() {
@@ -77,10 +96,7 @@ public class CadastroCozinhaIT {
 
     @Test
     public void shouldReturnHttpStatus200WhenGettingCozinhas() {
-        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
         RestAssured.given()
-                .basePath("/cozinhas")
-                .port(port)
                 .accept(ContentType.JSON)
                 .when()
                 .get()
@@ -90,16 +106,33 @@ public class CadastroCozinhaIT {
 
     @Test
     public void shouldReturnBodyWithFourCozinhas() {
-        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
         RestAssured.given()
-                .basePath("/cozinhas")
-                .port(port)
                 .accept(ContentType.JSON)
                 .when()
                 .get()
                 .then()
                 .statusCode(HttpStatus.OK.value())
-                .body("", Matchers.hasSize(4))
-                .body("nome", Matchers.hasItems("Indiana", "Tailandesa"));
+                .body("", Matchers.hasSize(1))
+                .body("nome", Matchers.hasItems("Chinesa"));
+    }
+
+    @Test
+    public void shouldCreateCozinhaAndReturnHttpStatus201() {
+        Cozinha cozinha = new Cozinha();
+        cozinha.setNome("Chinesa");
+        RestAssured.given()
+                .body(cozinha)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .when()
+                .post()
+                .then()
+                .statusCode(HttpStatus.CREATED.value());
+    }
+
+    private void prepareDatabase() {
+        Cozinha newCozinha = new Cozinha();
+        newCozinha.setNome("Chinesa");
+        Cozinha cozinha = cadastroCozinhaService.salvar(newCozinha);
     }
 }
