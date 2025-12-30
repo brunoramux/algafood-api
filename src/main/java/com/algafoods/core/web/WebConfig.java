6ca8b5cc67dba18c0eae6970cc78e9c23d753faf
Bruno Ramos Lemos <bruno.lemos@live.com>
2025-12-29 23:07:11 -0300

    Feat: documentacao com Springdoc - Swagger UI

--- src/main/java/com/algafoods/core/web/WebConfig.java
+++ src/main/java/com/algafoods/core/web/WebConfig.java
@@ -1,17 +1,43 @@
 package com.algafoods.core.web;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import jakarta.servlet.Filter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    // ADICIONA CORS - CROSS-ORIGIN PARA TODA A API
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
             .allowedOrigins("*")
             .allowedMethods("*");
     }
+
+    // Habilitar uso de ETAGS no cache HTTP
+    // ETAGS: É um cabeçalho HTTP usado para indicar a versão de um recurso.
+    // If-None-Match: "<etag>" -->>>>>>> SERVIDOR RESPONDE COM 304 CASO AS ETAGS SEJAM IGUAIS
+    @Bean
+    public Filter shallowEtagHeaderFilter() {
+        return new ShallowEtagHeaderFilter();
+    }
+
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Algafoods API")
+                        .description("Spring Boot API")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+    }
+
 
 }
